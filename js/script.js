c847ff196b478678c7be63ddc18b490f7b6ffea5
KoHyeongHo <unique_ho@daum.net>
2025-06-15 20:14:46 +0900

    Add files via upload
    Init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("btnProfile")
+  .addEventListener("click", () => window.location.href = "profile.html");
+document.getElementById("btnSkills")
+  .addEventListener("click", () => window.location.href = "skills.html");
+document.getElementById("btnProject")
+  .addEventListener("click", () => window.location.href = "project.html");
+document.getElementById("btnAwards")
+  .addEventListener("click", () => window.location.href = "awards.html");
+document.getElementById("btnEducation")
+  .addEventListener("click", () => window.location.href = "education.html");
+document.getElementById("btnOthers")
+  .addEventListener("click", () => window.location.href = "others.html");
